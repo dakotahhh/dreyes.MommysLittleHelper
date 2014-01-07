c509e897302cecd105de60408e9b38171b5333e1
dakotahhh <koteyec09@gmail.com>
2014-01-06 22:34:15 -0700

    created sha1 key for google services

--- DReyes_Code/src/dreyes/mommyslittlehelper/MainActivity.java
+++ DReyes_Code/src/dreyes/mommyslittlehelper/MainActivity.java
@@ -9,12 +9,12 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	PlusClient mPlusClient;
+//	PlusClient mPlusClient;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mPlusClient = new PlusClient.Builder(this,this,this).setActions("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity").build();
+//		mPlusClient = new PlusClient.Builder(this,this,this).setActions("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity").build();
 		setContentView(R.layout.activity_main);
 	}
 
@@ -30,35 +30,35 @@ public class MainActivity extends Activity {
 	{
 		Toast toast = Toast.makeText(getApplicationContext(), "Logging in using Google+", Toast.LENGTH_SHORT);
 		toast.show();
-		findViewById(R.id.sign_in_button).setOnClickListener(this);
+//		findViewById(R.id.sign_in_button).setOnClickListener(this);
 	}
 	
-	public void onClick(View view)
-	{
-		if(view.getId() == R.id.sign_in_button && !mPlusClient.isConnected())
-		{
-			if(mConnectionResult == null)
-			{
-				mConnectionProgressDialog.show();
-			}
-			else
-			{
-				try
-				{
-					mConnectionResult.startResolutionForResult(this,REQUEST_COE_RESOLVE_ERR);
-				}catch(SendIntentException e)
-				{
-					//Try connecting again
-					mConnectionResult = null;
-					mPlusClient.connect();
-				}
-			}
-		}
-	}
-	
-	public void onConnected(Bundle connectionHint)
-	{
-		mConectionProgressDialog.dismiss{);
-		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
-	}
+//	public void onClick(View view)
+//	{
+//		if(view.getId() == R.id.sign_in_button && !mPlusClient.isConnected())
+//		{
+//			if(mConnectionResult == null)
+//			{
+//				mConnectionProgressDialog.show();
+//			}
+//			else
+//			{
+//				try
+//				{
+//					mConnectionResult.startResolutionForResult(this,REQUEST_COE_RESOLVE_ERR);
+//				}catch(SendIntentException e)
+//				{
+//					//Try connecting again
+//					mConnectionResult = null;
+//					mPlusClient.connect();
+//				}
+//			}
+//		}
+//	}
+//	
+//	public void onConnected(Bundle connectionHint)
+//	{
+//		mConectionProgressDialog.dismiss{);
+//		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+//	}
 }
