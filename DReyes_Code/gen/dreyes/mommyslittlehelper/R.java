d3477baa8a28454f6e02fb09283ac07ec6c3c186
dakotahhh <koteyec09@gmail.com>
2014-01-08 21:14:06 -0700

    created project summary doc

--- DReyes_Code/gen/dreyes/mommyslittlehelper/R.java
+++ DReyes_Code/gen/dreyes/mommyslittlehelper/R.java
@@ -209,27 +209,33 @@ containing a value of this type.
         public static final int mlhlogo=0x7f02001d;
     }
     public static final class id {
-        public static final int action_settings=0x7f05000d;
-        public static final int button1=0x7f050005;
-        public static final int button2=0x7f050006;
-        public static final int button3=0x7f050007;
-        public static final int button4=0x7f050008;
+        public static final int action_settings=0x7f050012;
+        public static final int button1=0x7f05000b;
+        public static final int button2=0x7f05000c;
+        public static final int button3=0x7f05000d;
+        public static final int button4=0x7f05000e;
+        public static final int editText2=0x7f050009;
+        public static final int editText3=0x7f05000a;
         public static final int hybrid=0x7f050004;
-        public static final int imageView1=0x7f05000a;
+        public static final int imageView1=0x7f050010;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
-        public static final int sign_in_button=0x7f05000c;
-        public static final int sign_out_button=0x7f050009;
+        public static final int sign_in_button=0x7f050011;
+        public static final int sign_out_button=0x7f05000f;
         public static final int terrain=0x7f050003;
-        public static final int textView1=0x7f05000b;
+        public static final int textView1=0x7f050005;
+        public static final int textView2=0x7f050007;
+        public static final int textView3=0x7f050008;
+        public static final int textView4=0x7f050006;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_greetuser=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_fedbaby=0x7f030000;
+        public static final int activity_greetuser=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
