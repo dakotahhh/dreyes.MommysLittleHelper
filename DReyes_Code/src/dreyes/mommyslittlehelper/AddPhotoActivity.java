e1c238e2967cd14e9f12da81638599fc06273bab
dakotahhh <koteyec09@gmail.com>
2014-01-21 17:49:51 -0700

    initial code for uploading and adding an new album

--- DReyes_Code/src/dreyes/mommyslittlehelper/AddPhotoActivity.java
+++ DReyes_Code/src/dreyes/mommyslittlehelper/AddPhotoActivity.java
@@ -6,66 +6,94 @@ import android.os.Bundle;
 
 //imports for Picasa Web Album API
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import com.google.android.gms.plus.PlusClient;
-import com.google.gdata.client.GoogleService;
 import com.google.gdata.client.*;
+import com.google.gdata.client.http.AuthSubUtil;
 import com.google.gdata.client.photos.*;
 import com.google.gdata.data.*;
 import com.google.gdata.data.media.*;
 import com.google.gdata.data.photos.*;
+import com.google.gdata.util.AuthenticationException;
+import com.google.gdata.util.ServiceException;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class AddPhotoActivity extends Activity 
 {
+	PicasawebService myService;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		PicasawebService myService = new PicasawebService("exampleCo-exampleApp-1");
-		myService.setUserCredentials(username, password)
+		myService = new PicasawebService("exampleCo-exampleApp-1");
+		try {
+			myService.setUserCredentials("koteyec09@gmail.com", "bakashinji");
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}
+		String requestUrl = AuthSubUtil.getRequestUrl("http://www.example.com/RetrieveToken", "https://picasaweb.google.com/data", false, true);
+		String sessionToken = AuthSubUtil.exchangeForSessionToken(onetimeUseToken, null);
+		myService.setAuthSubToken(sessionToken,null);
+		
+		URL feedUrl = new URL("https://picasaweb.google.com/data/feed/api/user/username?kind=album");
+		UserFeed myUserFeed = myService.getFeed(feedUrl, UserFeed.class);
+		boolean exists = false;
+		for(AlbumEntry myAlbum :myUserFeed.getAlbumEntries())
+		{
+			if(myAlbum.getTitle().getPlainText().equals("MLH_Album"))
+			{
+				exists = true;
+			}
+		}
+		if(exists)
+		{
+			createNewAlbum();
+		}
+		else
+		{
+			uploadPhoto();
+		}
 	}
-//		super.onCreate(savedInstanceState);
-//		PicasawebService myService = new PicasawebService("exampleCo-exampleApp-1");
-//		//somehow get the account name do i really need this? probably not
-//		URL feedUrl = new URL("https://picasaweb.google.com/data/feed/api/user/username?kind=album");
-//		UserFeed myUserFeed = myService.getFeed(feedUrl, UserFeed.class);
-//		boolean exists = false;
-//		for(AlbumEntry myAlbum: myUserFeed.getAlbumEntries())
-//		{
-//			if(myAlbum.getName().equals("MLH Album"))
-//			{
-//				exists = true;
-//			}
-//		}
-//		if(exists)
-//		{
-//			createNewAlbum();
-//		}
-//	}
-//	
-//	public void createNewAlbum()
-//	{
-//		AlbumEntry mlhAlbum = new AlbumEntry();
-//		mlhAlbum.setName("MLH Album");
-//		mlhAlbum.setDescription("Baby pictures");
-//		
-//		
-//	}
-//	
-//	public void uploadPhoto()
-//	{
-//		URL albumPostUrl = new URL("https://picasaweb.google.com/data/feed/api/user/username/albumid/albumid");
-//		
-//		PhotoEntry myPhoto = new PhotoEntry();
-//		
-//		myPhoto.setDescription("yay a baby");
-//		myPhoto.setClient("myClientName");
-//		
-//		MediaFileSource photo = new MediaFileSource(new File("aldf.jpg"), "image/jpeg");
-//		myPhoto.setMediaSource(photo)
-//	}
+	
+	public void createNewAlbum()
+	{
+		AlbumEntry mlhAlbum = new AlbumEntry();
+		mlhAlbum.setTitle(new PlainTextConstruct("MLH Album"));
+		mlhAlbum.setDescription(new PlainTextConstruct("my baby"));
+		AlbumEntry insertedEntry = myService.insert(postUrl, mlhAlbum);
+	}
+	
+	public void uploadPhoto()
+	{
+		URL albumPostUrl = null;
+		try {
+			albumPostUrl = new URL("https://picasaweb.google.com/data/feed/api/user/koteyec09/albumid/MLH_Album");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		PhotoEntry myPhoto = new PhotoEntry();
+		myPhoto.setTitle(new PlainTextConstruct("look at my beautiful baby"));
+		myPhoto.setDescription(new PlainTextConstruct("looka thim"));
+		myPhoto.setClient("myClientName");
+		
+		MediaFileSource photo = new MediaFileSource(new File("aldf.jpg"), "image/jpeg");
+		myPhoto.setMediaSource(photo);
+		
+		try {
+			PhotoEntry returnedPhoto = myService.insert(albumPostUrl, myPhoto);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
