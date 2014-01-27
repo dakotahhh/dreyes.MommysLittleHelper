7c732e2324604fa0444d4ddf4597a767bbacab5c
dakotahhh <koteyec09@gmail.com>
2014-01-27 13:13:00 -0700

    first changes to logging in

--- DReyes_Code/src/dreyes/mommyslittlehelper/FacebookAddPhotoActivity.java
+++ DReyes_Code/src/dreyes/mommyslittlehelper/FacebookAddPhotoActivity.java
@@ -12,46 +12,40 @@ import android.content.*;
 
 public class FacebookAddPhotoActivity extends Activity 
 {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_facebookaddphoto);
-		//Start facebook login
-		Session.openActiveSession(this, true, new Session.StatusCallback() {
-			//callback when session changes state
-			@Override
-			public void call(Session session, SessionState state, Exception exception) {
-				if(session.isOpened())
-				{
-					//request to the /me API
-					Request.newMeRequest(session, new Request.GraphUserCallback() {
-						//callbacka fter graph api response with user object
-						@Override
-						public void onCompleted(GraphUser user, Response response) {
-							if(user!=null)
-							{
-								TextView welcome = (TextView)findViewById(R.id.welcome);
-								welcome.setText("Hello " + user.getName() + "!");
-								Log.d("USERNAME", user.getName());
-							}
-							else
-							{
-								TextView welcome = (TextView)findViewById(R.id.welcome);
-								welcome.setText("Could not find user name");
-								Log.d("USERNAME", "not found");
-							}
-						}
-					}).executeAsync();
-				}
-				Log.d("session is not open", "hello");
-			}
-		});
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-	}
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_facebookaddphoto);
+
+	    // start Facebook Login
+	    Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+	      // callback when session changes state
+	      @Override
+	      public void call(Session session, SessionState state, Exception exception) {
+	        if (session.isOpened()) {
+
+	          // make request to the /me API
+	          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+	            // callback after Graph API response with user object
+	            @Override
+	            public void onCompleted(GraphUser user, Response response) {
+	              if (user != null) {
+	                TextView welcome = (TextView) findViewById(R.id.welcome);
+	                welcome.setText("Hello " + user.getName() + "!");
+	              }
+	            }
+	          });
+	        }
+	      }
+	    });
+	  }
+
+	  @Override
+	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	  }
 
 }
