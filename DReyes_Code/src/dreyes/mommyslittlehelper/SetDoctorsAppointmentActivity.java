beb9e5da3527506a1765606eed2771638b09d7f5
dakotahhh <koteyec09@gmail.com>
2014-02-05 13:18:23 -0700

    creating datepicker initial info

--- DReyes_Code/src/dreyes/mommyslittlehelper/SetDoctorsAppointmentActivity.java
+++ DReyes_Code/src/dreyes/mommyslittlehelper/SetDoctorsAppointmentActivity.java
@@ -1,7 +1,47 @@
 package dreyes.mommyslittlehelper;
 
+
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class SetDoctorsAppointmentActivity extends Activity {
 
+	private TextView currentDate;
+	private DatePicker datePicker;
+	
+	private int year, month, day;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setdoctorsappointment);
+		
+		currentDate = (TextView)findViewById(R.id.currentDate);
+		datePicker = (DatePicker)findViewById(R.id.datePicker);
+		
+		setCurrentDateOnView();
+		
+	}
+	
+	private void setCurrentDateOnView()
+	{
+		final Calendar c = Calendar.getInstance();
+		year = c.get(Calendar.YEAR);
+		month = c.get(Calendar.MONTH);
+		day = c.get(Calendar.DAY_OF_MONTH);
+		
+		currentDate.setText(new StringBuilder()
+				.append(month + 1)
+				.append("-")
+				.append(day)
+				.append("-")
+				.append(year)
+				.append(" "));
+		datePicker.init(year, month, day, null);
+	}
+	
 }
