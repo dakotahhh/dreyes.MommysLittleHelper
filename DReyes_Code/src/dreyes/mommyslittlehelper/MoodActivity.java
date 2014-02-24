1db03d5c1f605f4a337b96114bb1054780dcb477
dakotahhh <koteyec09@gmail.com>
2014-02-24 16:07:54 -0700

    creating .xml pages for moods

--- DReyes_Code/src/dreyes/mommyslittlehelper/MoodActivity.java
+++ DReyes_Code/src/dreyes/mommyslittlehelper/MoodActivity.java
@@ -1,5 +1,88 @@
 package dreyes.mommyslittlehelper;
 
-public class MoodActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ViewFlipper;
 
+public class MoodActivity extends Activity{
+
+	private ViewFlipper vf;
+	private float oldTouchValue;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_moods);
+		vf = (ViewFlipper)findViewById(R.id.ViewFlipper01);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			oldTouchValue = event.getX();
+			break;
+		case MotionEvent.ACTION_UP:
+			float currentX = event.getX();
+			if(oldTouchValue < currentX)
+			{
+				vf.setInAnimation(inFromLeftAnimation());
+				vf.setOutAnimation(outToRightAnimation());
+				vf.showNext();
+			}
+			if(oldTouchValue > currentX)
+			{
+				vf.setInAnimation(inFromRightAnimation());
+				vf.setOutAnimation(outToLeftAnimation());
+				vf.showPrevious();
+			}
+			break;
+		}
+		return false;
+	}
+	
+	public static Animation inFromRightAnimation()
+	{
+		Animation inFromRight = new TranslateAnimation
+				(Animation.RELATIVE_TO_PARENT, +1.0f, Animation.RELATIVE_TO_PARENT, 0.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f);
+		inFromRight.setDuration(350);
+		inFromRight.setInterpolator(new AccelerateInterpolator());
+		return inFromRight;
+	}
+	
+	public static Animation outToLeftAnimation()
+	{
+		Animation outtoLeft = new TranslateAnimation
+				(Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, -1.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f);
+		outtoLeft.setDuration(350);
+		outtoLeft.setInterpolator(new AccelerateInterpolator());
+		return outtoLeft;
+	}
+	
+	public static Animation inFromLeftAnimation()
+	{
+		Animation inFromLeft = new TranslateAnimation
+				(Animation.RELATIVE_TO_PARENT, -1.0f, Animation.RELATIVE_TO_PARENT, -1.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f);
+		inFromLeft.setDuration(350);
+		inFromLeft.setInterpolator(new AccelerateInterpolator());
+		return inFromLeft;
+	}
+	
+	public static Animation outToRightAnimation()
+	{
+		Animation outtoRight = new TranslateAnimation
+				(Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, +1.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f);
+		outtoRight.setDuration(350);
+		outtoRight.setInterpolator(new AccelerateInterpolator());
+		return outtoRight;
+	}
+	
 }
